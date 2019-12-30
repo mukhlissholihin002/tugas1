5dba4c40cc59daa427929442bb8a21ffc97c14e1
mukhlissholihin002 <mukhlissholihin423@gmail.com>
2019-12-30 10:47:35 +0700

    Tugas Akhir

--- app/src/main/java/com/example/profilmukhlis/MainActivity.java
+++ app/src/main/java/com/example/profilmukhlis/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.profilmukhlis;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn_setting;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_setting = (Button)findViewById(R.id.button_setting);
+
+        btn_setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Settings.ACTION_LOCALE_SETTINGS);
+                startActivity(intent);
+            }
+        });
     }
+
 }
